6dfcf9b87795b4ffb5c0c5275a85d1ef10812204
gravityfargo <gravityfargo@gmail.com>
2023-09-20 11:31:12 -0400

    modified models and sample data for new fields

--- tasks/models.py
+++ tasks/models.py
@@ -1,31 +1,32 @@
 import datetime
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
-
-# Create your models here.
 class Tag(models.Model):
-    # attributes
     tag_text = models.CharField(max_length=10, unique=True, blank=True)
-    date_created = models.DateField()
-    # method to return a human readable represention of the object
+    date_created = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    date_modified = models.DateTimeField(auto_now_add = True, blank=True)
+    created_by_user =  models.CharField(max_length = 180, blank=True)
+    
     def __str__(self):
         return self.tag_text
     
-    # custom method to return the time delta since creation
     def days_since_creation(self):
         return self.date_created >= timezone.now() - datetime.timedelta(days=1)
 
-    # custom method to return the time delta with regards to the due date
     def days_away_from_due(self):
         return self.date_due >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Task(models.Model):
-    task_text = models.CharField(max_length=50)
-    task_tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    date_created = models.DateField()
-    date_due = models.DateField()
-
+    task_text = models.CharField(max_length = 180, blank=True)
+    task_tag = models.ForeignKey(Tag, on_delete = models.CASCADE, null = True)
+    date_created = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    date_due = models.DateTimeField(auto_now = True, null=True)
+    date_modified = models.DateTimeField(auto_now = True, blank = True)
+    created_by_user =  models.CharField(max_length = 180, blank=True)
+    completedstatus = models.BooleanField(default = False, blank = True)
+    
     def __str__(self):
         return self.task_text
