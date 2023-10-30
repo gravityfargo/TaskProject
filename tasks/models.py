5d531a5e3ef5c55124b13a9241acb85d95fe7b54
Nathan Price <gravityfargo@gmail.com>
2023-10-30 12:42:44 -0400

    update models, remove old tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,20 +1,41 @@
-import imp
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 class Task(models.Model):
     # cascade if user deleted, will deleted its tasks
-    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length = 200, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default = False)
-    created = models.DateTimeField(auto_now_add = True)
+    tag = models.CharField(max_length=200, null=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    modified = models.DateTimeField(auto_now_add=False, null=True, blank=True)
 
     def __str__(self):
         return self.title
     
-    # Model metadata is “anything that’s not a field”, 
-    # such as ordering options (like i.e. sorting), 
-    # database table name (db_table), 
+    def days_since_creation(self):
+        return self.created >= timezone.now() - datetime.timedelta(days=1)
+
+    def days_away_from_due(self):
+        return self.due >= timezone.now() - datetime.timedelta(days=1)
+
+    # Model metadata is “anything that’s not a field”,
+    # such as ordering options (like i.e. sorting),
+    # database table name (db_table),
     # or human-readable singular and plural names
     class Meta:
-        ordering = ['complete']
+        ordering = ["complete"]
+
+class Tag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    modified = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+  
+    def __str__(self):
+        return self.title
--- tasks/urls.py
+++ tasks/urls.py
@@ -5,10 +5,18 @@ urlpatterns = [
     # name='' is used for routing buttons and shit
     # <a href="{% url 'task-detail' %}">
 
-    # by default loads the task_list.html template
+    # urls for templates
     path('', TaskList.as_view(), name='task'),
     path("task-create/", TaskCreate.as_view(), name="task-create"),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete')
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+
+    # urls for api calls
+    # temp copy paste as placeholders
+    path('', TaskList.as_view(), name='task'),
+    path("api/task-create/", TaskCreate.as_view(), name="task-create"),
+    path('api/task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('api/task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('api/task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
