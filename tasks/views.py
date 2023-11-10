89abc114862834eead4d9fbcaefff147d9324257
Nathan Price <gravityfargo@gmail.com>
2023-11-09 20:26:01 -0500

    user management requred for class views
    - prepopulate forms with authd user
    - only show needed form fields

--- tasks/views.py
+++ tasks/views.py
@@ -1,36 +1,56 @@
+from typing import Any, Dict, Set
+from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
 # class based views, create a new object that inherets django provided classes
 # specificlly for present model data
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     # contextobjectname is the object you can pull data from in the template
     # {% for task in tasks %}
     context_object_name = 'tasks'
-    
-class TaskDetail(DetailView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # when the page is loaded, this class will take the 'task' model object (task table in the database)
+        # this filter only allow tasks for the logged in user who sent the request (loaded the page)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'tasks/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     # createview shows a form
     model = Task
-    # can use a list ['field1', ...]
-    fields = '__all__'
+    # which fields are shown to the user
+    fields = ['title', 'description', 'tag', 'due']
     # template name from urls
     success_url = reverse_lazy('task')
 
-class TaskUpdate(UpdateView):
+    # sets the logged in user as the 'user' for the task 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'tag', 'due']
     success_url = reverse_lazy('task')
 
-class TaskDelete(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('task')
