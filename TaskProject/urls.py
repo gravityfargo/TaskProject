69cab7b50c793bb63df303aa4c6259556c7f4951
Nathan Price <gravityfargo@gmail.com>
2023-11-13 12:12:28 -0500

    implemented user registration

--- TaskProject/urls.py
+++ TaskProject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from .views import GlobalLogin
+from .views import GlobalLogin, GlobalRegister
  
 urlpatterns = [
     # admin panel is a built in django feature
@@ -10,10 +10,10 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="dashboard.html"), name="dashboard"),
     path('tasks/', include('tasks.urls')),
     path('login/', GlobalLogin.as_view(), name="login"),
+    path('register/', GlobalRegister.as_view(), name="register"),
     # next_page is the page user is sent to after logging out
     path('logout/', LogoutView.as_view(next_page='dashboard'), name="logout")
 
-    # path("accounts/", include("django.contrib.auth.urls")),
-    # path("register/", taskviews.register, name="register"),
+    # path("accounts/", include("django.contrib.auth.urls")),,
     # path('tasks/', include("tasks.urls")),
 ]
--- TaskProject/views.py
+++ TaskProject/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
-
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 class GlobalLogin(LoginView):
     template_name = 'login.html'
     fields = "__all__"
@@ -8,3 +10,16 @@ class GlobalLogin(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('dashboard')
+
+class GlobalRegister(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+
+        return super(GlobalRegister, self).form_valid(form)
