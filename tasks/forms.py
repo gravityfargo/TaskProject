4fc309db5d90f940dbbcada4fc5da8180a4304c6
Nathan Price <gravityfargo@gmail.com>
2023-11-20 17:01:06 -0500

    task form overhaul TaskList() task detail view now grabs the right task TaskCreate has been moved to a class based FormView and ModelForm TaskUpdate now uses class based ModelForm formatted all the things

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,12 +1,20 @@
-from django.forms import TextInput, CharField
+from django.forms import TextInput
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django import forms
-from .models import Tag
+from .models import Tag, Task
+
 
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
-        fields = ['title', 'color']
-        exclude = ('user',)
-        widgets = {
-            "color": TextInput(attrs={"type": "color"})
-        }
+        fields = ["title", "color"]
+        exclude = ("user",)
+        widgets = {"color": TextInput(attrs={"type": "color"})}
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "description", "tag", "due"]
+        exclude = ("user",)
+        widgets = {"due": DatePickerInput()}
--- tasks/views.py
+++ tasks/views.py
@@ -1,25 +1,25 @@
-from bootstrap_datepicker_plus.widgets import DatePickerInput
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import ListView, UpdateView, DeleteView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task, Tag
-from .forms import TagForm
+from .forms import TagForm, TaskForm
 
 
 # class based views, create a new object that inherets django provided classes
 # specificlly for present model data
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
 
     # contextobjectname is the object you can pull data from in the template
     # {% for task in tasks %}
-    context_object_name = 'tasks'
-    template_name = 'tasks/task_list.html'
-    
+    context_object_name = "tasks"
+    template_name = "tasks/task_list.html"
+
     def get(self, request, *args, **kwargs):
         # checks to see if a task primary key was included in the url
-        self.taskDetailID = kwargs.get('pk')
+        self.taskDetailID = kwargs.get("pk")
         return super(TaskList, self).get(request, *args, **kwargs)
 
     # Used by ListViews - it determines the list of objects that you want to display
@@ -28,77 +28,75 @@ class TaskList(LoginRequiredMixin, ListView):
         qs = super().get_queryset()
 
         # ** each key-value pair of the dict is passed as a keyword argument
-        self.queryfilters = { 'user': self.request.user }
+        self.queryfilters = {"user": self.request.user}
         return qs.filter(**self.queryfilters)
 
     def get_context_data(self, **kwargs):
         # used to populate a dictionary to use as the template context. ListViews will populate the result from get_queryset's return
         context = super().get_context_data(**kwargs)
 
-        context['incomplete'] = context['tasks'].filter(complete=False).count()
-        context['tags'] = Tag.objects.filter(user=self.request.user)
-        for tag in context['tags']:
+        context["incomplete"] = context["tasks"].filter(complete=False).count()
+        context["tags"] = Tag.objects.filter(user=self.request.user)
+        for tag in context["tags"]:
             # execute the method for each tag to get its updated count
             tag.count(self.request.user)
 
         daystoday = 0
-        daysnextseven = 0  
-        for task in context['tasks']:
+        daysnextseven = 0
+        for task in context["tasks"]:
             if task.days_away_from_due() == 0:
                 daystoday += 1
             if task.days_away_from_due() <= 7:
                 daysnextseven += 1
-        context['due_today'] = daystoday
-        context['due_next_seven_days'] = daysnextseven
+        context["due_today"] = daystoday
+        context["due_next_seven_days"] = daysnextseven
 
         if self.taskDetailID != None:
-            pk = self.taskDetailID-  1
-            context['task_for_detail_view'] = context['tasks'][pk]
+            pk = self.taskDetailID
+            for task in context["tasks"]:
+                if task.pk == pk:
+                    context["task_for_detail_view"] = task
         else:
-            context['task_for_detail_view'] = "None"
+            context["task_for_detail_view"] = "None"
 
         return context
 
-class TaskDetail(LoginRequiredMixin, DetailView):
-    model = Task
-    context_object_name = 'task'
-    template_name = 'tasks/task.html'
-class TaskCreate(LoginRequiredMixin, CreateView):
-    # createview shows a form
-    model = Task
-    # which fields are shown to the user
-    fields = ['title', 'description', 'tag', 'due']
-    # template name from urls
-    success_url = reverse_lazy('task')
 
-    # sets the logged in user as the 'user' for the task 
+class TaskCreate(LoginRequiredMixin, FormView):
+    template_name = "tasks/task_form.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("task")
+
     def form_valid(self, form):
         form.instance.user = self.request.user
+        form.save()
         return super(TaskCreate, self).form_valid(form)
-    
-    def get_form(self, form_class=None):
-        form = super(TaskCreate, self).get_form(form_class)
-        form.fields["due"].widget = DatePickerInput()
-        return form
+
+
 class TaskUpdate(LoginRequiredMixin, UpdateView):
+    template_name = "tasks/task_form.html"
+    form_class = TaskForm
     model = Task
-    fields = ['title', 'description', 'tag', 'due']
-    success_url = reverse_lazy('task')
+    success_url = reverse_lazy("task")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(TaskCreate, self).form_valid(form)
+        form.save()
+        return super(TaskUpdate, self).form_valid(form)
+
+
 class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
-    context_object_name = 'task'
-    success_url = reverse_lazy('task')
+    context_object_name = "task"
+    success_url = reverse_lazy("task")
+
 
 class TagCreate(LoginRequiredMixin, FormView):
     template_name = "tasks/tag_form.html"
     form_class = TagForm
-    success_url = reverse_lazy('task')
+    success_url = reverse_lazy("task")
 
-    # sets the logged in user as the 'user' for the tag 
+    # sets the logged in user as the 'user' for the tag
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.save()
