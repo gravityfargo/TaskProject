30c5585c66642c4fb8a313994207dbe253c2a62a
Nathan Price <gravityfargo@gmail.com>
2023-11-20 16:56:45 -0500

    removed task detail view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TagCreate
+from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete, TagCreate
 
 urlpatterns = [
     # name='' is used for routing buttons and shit
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', TaskList.as_view(), name="task"),
     path('<int:pk>/', TaskList.as_view(), name='task-with-detail'),
     path("task-create/", TaskCreate.as_view(), name="task-create"),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
     path("tag-create/", TagCreate.as_view(), name="tag-create"),
