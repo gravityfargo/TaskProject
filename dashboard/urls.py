8fb546c9f2bc1215f0773b3d9a4b46c3d5e827fd
Nathan Price <gravityfargo@gmail.com>
2023-11-24 12:39:26 -0500

    cleaned imports

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,4 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LogoutView
+from django.urls import path
 from .views import GlobalLogin, GlobalRegister, DashboardView
 
 urlpatterns = [
