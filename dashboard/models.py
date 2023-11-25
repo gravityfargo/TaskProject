1e3ed2c85ce6ea2b579d2629ee7e9624b98cc402
Nathan Price <gravityfargo@gmail.com>
2023-11-24 19:53:12 -0500

    endpoint nav setting

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dashboard_modules = models.TextField(max_length=500, blank=True)
     task_sort_order = models.CharField(max_length=30, blank=True)
+    show_api_endpoints = models.BooleanField(default=False)
     
     # define signals so our Profile model will be automatically created/updated when we create/update User instances. 
     # Basically we are hooking the create_user_profile and save_user_profile methods to the User model, 
