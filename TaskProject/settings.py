f5696e7dc0cc58a1216f30924052d2c96c4e7eab
Nathan Price <gravityfargo@gmail.com>
2023-11-29 11:53:00 -0500

    migrate to the correct bootstrap package

--- TaskProject/settings.py
+++ TaskProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
     'dashboard.apps.DashboardConfig',
-    'bootstrap5',
+    'django_bootstrap5',
     'bootstrap_datepicker_plus',
     'rest_framework',
 ]
